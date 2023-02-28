4a7711b9160ee1f7e8de6649d58ad8752fb22c0e
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2023-02-28 17:11:12 +0200

    nav menu with styles

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -6,15 +6,49 @@ import LogoutIcon from '@mui/icons-material/Logout'
 
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark'
 import { AppBar, IconButton, Toolbar } from '@mui/material'
+import { styled } from '@mui/material/styles'
 import Box from '@mui/system/Box'
+import Container from '@mui/system/Container'
 import Stack from '@mui/system/Stack'
+import Link from 'next/link'
 
-// const StyledToolbar = styled(Toolbar)({
-//   display: 'flex',
-//   '@media all': {
-//     minHeight: 85,
-//   },
-// })
+const StyledLink = styled(Link)(({ theme }) => ({
+  display: 'inline-block',
+  fontWeight: 500,
+  fontSize: '1.2rem',
+  lineHeight: 1.75,
+  color: theme.palette.primary.main,
+  textDecoration: 'none',
+  paddingBottom: '4px',
+  position: 'relative',
+  transition: '.2s',
+
+  ':after': {
+    display: 'block',
+    transition: '.2s',
+    position: 'absolute',
+    content: '""',
+    height: '3px',
+    width: '0',
+    bottom: '0',
+    left: '50%',
+    backgroundColor: theme.palette.secondary.main,
+  },
+
+  '&:first-of-type': {
+    [theme.breakpoints.up('md')]: {
+      marginLeft: 0,
+    },
+  },
+
+  '&:hover, &.active': {
+    color: theme.palette.secondary.dark,
+    ':after': {
+      width: '100%',
+      left: '0',
+    },
+  },
+}))
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
@@ -38,7 +72,27 @@ export default function MainLayout({ children }: { children: ReactNode }) {
             </Stack>
           </Toolbar>
         </AppBar>
-
+        <Container
+          component="nav"
+          maxWidth="lg"
+          disableGutters
+          sx={{
+            minHeight: 60,
+            display: 'flex',
+            alignItems: 'flex-end',
+          }}
+        >
+          <Stack
+            direction={{ xs: 'column', sm: 'row' }}
+            pt={{ xs: 2, sm: 0 }}
+            spacing={{ xs: 1, sm: 4 }}
+          >
+            <StyledLink href="/">Public</StyledLink>
+            <StyledLink href="/private">Private</StyledLink>
+            <StyledLink href="/shared">Shared</StyledLink>
+            <StyledLink href="/archived">Archived</StyledLink>
+          </Stack>
+        </Container>
         {children}
       </Box>
     </>
