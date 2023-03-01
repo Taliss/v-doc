b6e8bd3eb587d0f531d619f49eef91a222ec3f10
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2023-03-01 17:55:07 +0200

    protect personal page

--- src/pages/personal.tsx
+++ src/pages/personal.tsx
@@ -1,4 +1,24 @@
 import { getLayout } from '@/layouts/MainLayout'
+import { getServerSession } from 'next-auth'
+import { GetServerSideProps } from 'next/types'
+import routes from 'routes'
+import { authOptions } from './api/auth/[...nextauth]'
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  //For getting session on server side the docs recommend using getServerSession as per
+  //here: https://next-auth.js.org/configuration/nextjs#getserversession
+  const session = await getServerSession(ctx.req, ctx.res, authOptions)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: routes.root,
+        permanent: false,
+      },
+    }
+  }
+  return { props: { session } }
+}
 
 export default function Personal() {
   return <h1>Helloo there from personal!</h1>
