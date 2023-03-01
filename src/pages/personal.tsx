77e2cfddbcdbd97ffadbe9ce50e070444d40f720
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2023-03-01 14:38:51 +0200

    debug/dev: temporal personal page with session

--- src/pages/personal.tsx
+++ src/pages/personal.tsx
@@ -1,25 +1,11 @@
-// export async function getServerSideProps() {
-//   const files = await prisma.file.findMany({
-//     where: { authorId: '7802b1d1-395f-4e21-939f-a9f74ed1ee9a' },
-//     include: {
-//       owner: {
-//         select: { email: true },
-//       },
-//     },
-//   })
-
-//   return {
-//     props: {
-//       files,
-//     },
-//   }
-// }
+import { useSession } from 'next-auth/react'
 
 export default function Personal() {
+  const { data: session } = useSession()
   return (
     <>
       <h2>Helloo</h2>
-      <p>I see a protected route here!</p>
+      <p>{JSON.stringify(session)}</p>
     </>
   )
 }
