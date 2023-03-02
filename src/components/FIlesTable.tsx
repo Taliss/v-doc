1b326f07ac2a63c00aab6fd420dc5c046c1f8633
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2023-03-02 16:37:57 +0200

    wip: initial table

---
+++ src/components/FIlesTable.tsx
@@ -0,0 +1,103 @@
+import { TextSnippet } from '@mui/icons-material'
+import { IconButton, Paper, TableBody, Tooltip, Typography } from '@mui/material'
+import Box from '@mui/material/Box'
+import Table from '@mui/material/Table'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
+
+type SimpleFile = {
+  id: string
+  name: string
+  createdAt: string
+  updatedAt: string
+  visibility: string
+  owner: {
+    email: string
+  }
+}
+
+type Props = {
+  rows: SimpleFile[]
+}
+
+const HeaderTextCell = ({ label }: { label: string }) => (
+  <TableCell>
+    <Typography noWrap fontSize={16} fontWeight="bold" color="primary">
+      {label.toUpperCase()}
+    </Typography>
+  </TableCell>
+)
+
+const ActionsCell = ({ visibility }: { visibility: string }) => {
+  const Icon = visibility.toLowerCase() === 'public' ? VisibilityIcon : VisibilityOffIcon
+  const title =
+    visibility.toLowerCase() === 'public' ? 'Switch file to private' : 'Switch file to public'
+
+  return (
+    <TableCell>
+      <Tooltip title={title}>
+        <IconButton color="secondary" onClick={() => console.log('HELLO')}>
+          <Icon />
+        </IconButton>
+      </Tooltip>
+    </TableCell>
+  )
+}
+
+export default function FilesTable({ rows }: Props) {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="file-listing">
+        <TableHead>
+          <TableRow>
+            <HeaderTextCell label="name" />
+            <HeaderTextCell label="author" />
+            <HeaderTextCell label="created at" />
+            <HeaderTextCell label="updated at" />
+            {/* Empty cell for horz-menu (extending t-header length) */}
+            <TableCell />
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                <Box
+                  sx={{
+                    display: 'inline-block',
+                    marginRight: '20px',
+                    verticalAlign: 'middle',
+                  }}
+                >
+                  <TextSnippet color="primary" />
+                </Box>
+                {row.name}
+              </TableCell>
+              {/* <TableCell>
+                <Box sx={{ display: 'flex' }}>
+                  <>
+                    {row.sharedWith.slice(0, 7).map((user, index) => {
+                      return <UserAvatar key={index} user={user} />
+                    })}
+                    {row.sharedWith.length > 7 && (
+                      <ShowMoreUsersTooltip leftOverUsers={row.sharedWith.slice(7)} />
+                    )}
+                  </>
+                </Box>
+              </TableCell> */}
+              <TableCell>{row.owner.email}</TableCell>
+              <TableCell>{row.createdAt}</TableCell>
+              <TableCell>{row.updatedAt}</TableCell>
+              <ActionsCell visibility={row.visibility} />
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
--- src/pages/personal.tsx
+++ src/pages/personal.tsx
@@ -1,3 +1,4 @@
+import FilesTable from '@/components/FIlesTable'
 import { getLayout } from '@/layouts/MainLayout'
 import { getServerSession } from 'next-auth'
 import { GetServerSideProps } from 'next/types'
@@ -19,8 +20,41 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   return { props: { session } }
 }
 
+const data = [
+  {
+    id: 'c6c69739-9f3b-455f-9dc0-aa39be88c9aa',
+    name: 'one more time',
+    visibility: 'PUBLIC',
+    updatedAt: '2023-03-02T13:30:06.462Z',
+    createdAt: '2023-03-02T13:30:06.462Z',
+    owner: {
+      email: 'ivv@yopmail.com',
+    },
+  },
+  {
+    id: '377ca9c7-6836-4580-8381-239404060239',
+    name: 'bb',
+    visibility: 'PUBLIC',
+    updatedAt: '2023-03-02T13:23:33.328Z',
+    createdAt: '2023-03-02T13:23:33.328Z',
+    owner: {
+      email: 'ivv@yopmail.com',
+    },
+  },
+  {
+    id: '2de644cc-2e81-4002-a933-9c26e88a8151',
+    name: 'ahaa',
+    visibility: 'PRIVATE',
+    updatedAt: '2023-03-02T13:22:24.914Z',
+    createdAt: '2023-03-02T13:22:24.914Z',
+    owner: {
+      email: 'ivv@yopmail.com',
+    },
+  },
+]
+
 export default function Personal() {
-  return <h1>Helloo there from personal!</h1>
+  return <FilesTable rows={data}></FilesTable>
 }
 
 Personal.getLayout = getLayout
