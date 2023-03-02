6b16fdac7b9ae40a25492758d9d19997a70e6829
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2023-03-02 11:09:01 +0200

    create new file dialog

--- src/components/popovers/CreateFileDialog.tsx
+++ src/components/popovers/CreateFileDialog.tsx
@@ -1,7 +1,12 @@
 import { yupResolver } from '@hookform/resolvers/yup'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import Radio from '@mui/material/Radio'
 import Stack from '@mui/material/Stack'
 import { UseConfirmProps } from 'hooks/useConfirm'
-import { useRouter } from 'next/router'
+import { useState } from 'react'
 import { FormProvider, useForm, UseFormProps } from 'react-hook-form'
 import { object, string } from 'yup'
 import ControlTextField from '../auth/ControlTextField'
@@ -11,7 +16,6 @@ type FormData = {
   fileName: string
   fileContent: string
 }
-
 const formOptions: UseFormProps<FormData> = {
   mode: 'onBlur',
   defaultValues: { fileName: '', fileContent: '' },
@@ -30,9 +34,9 @@ const formOptions: UseFormProps<FormData> = {
 }
 
 export default function CreateFileDialog({ open, closeHandler }: UseConfirmProps) {
-  const router = useRouter()
   const methods = useForm<FormData>(formOptions)
-  const { setValue, setError } = methods
+  const [visibility, setVisibility] = useState<'private' | 'public'>('private')
+  // const { setValue, setError } = methods
 
   const onSubmit = async (formData: FormData) => {
     // TODO send form actually
@@ -42,34 +46,74 @@ export default function CreateFileDialog({ open, closeHandler }: UseConfirmProps
   }
 
   return (
-    <>
-      <FormProvider {...methods}>
-        <GenericDialog<FormData>
-          closeHandler={closeHandler}
-          open={open}
-          primaryLabel="Create"
-          title="New File"
-          onSubmit={onSubmit}
-          dialogProps={{
-            maxWidth: 'md',
-            sx: {
-              '& .MuiDialog-container': {
-                height: '85%',
-              },
+    <FormProvider {...methods}>
+      <GenericDialog<FormData>
+        closeHandler={closeHandler}
+        open={open}
+        primaryLabel="Create"
+        title="New File"
+        onSubmit={onSubmit}
+        dialogProps={{
+          maxWidth: 'md',
+          sx: {
+            '& .MuiDialog-container': {
+              height: '85%',
             },
-          }}
-        >
-          <Stack spacing={1}>
-            <ControlTextField name="fileName" placeholder="Name your file" autoFocus />
+          },
+        }}
+      >
+        <Stack spacing={1}>
+          <Stack direction="row" justifyContent="space-between">
             <ControlTextField
-              name="fileContent"
-              placeholder="File Content"
-              multiline
-              minRows={14}
+              name="fileName"
+              placeholder="Name your file"
+              autoFocus
+              sx={{ maxWidth: 400 }}
             />
+            <Stack direction="row" spacing={2}>
+              <ListItem disablePadding>
+                <ListItemButton
+                  disableRipple
+                  sx={{ py: 0 }}
+                  onClick={() => {
+                    setVisibility('private')
+                  }}
+                >
+                  <ListItemIcon sx={{ minWidth: '0.5rem' }}>
+                    <Radio
+                      edge="start"
+                      tabIndex={-1}
+                      disableRipple
+                      checked={visibility === 'private'}
+                    />
+                  </ListItemIcon>
+                  <ListItemText primary="Private" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton
+                  disableRipple
+                  sx={{ py: 0 }}
+                  onClick={() => {
+                    setVisibility('public')
+                  }}
+                >
+                  <ListItemIcon sx={{ minWidth: '0.5rem' }}>
+                    <Radio
+                      edge="start"
+                      tabIndex={-1}
+                      disableRipple
+                      checked={visibility === 'public'}
+                    />
+                  </ListItemIcon>
+                  <ListItemText primary="Public" />
+                </ListItemButton>
+              </ListItem>
+            </Stack>
           </Stack>
-        </GenericDialog>
-      </FormProvider>
-    </>
+          <ControlTextField name="fileContent" placeholder="File Content" multiline minRows={14} />
+        </Stack>
+      </GenericDialog>
+    </FormProvider>
   )
 }
