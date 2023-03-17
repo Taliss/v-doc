37cc09774fcf2481a39b817e9a2d74f4f053b136
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2023-03-17 11:50:01 +0200

    use next-auth middleware to secure pages

--- src/pages/personal.tsx
+++ src/pages/personal.tsx
@@ -4,21 +4,12 @@ import axios, { AxiosResponse } from 'axios'
 import { getServerSession } from 'next-auth'
 import { GetServerSideProps } from 'next/types'
 import { useEffect, useState } from 'react'
-import routes from 'routes'
 import { authOptions } from './api/auth/[...nextauth]'
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   //For getting session on server side the docs recommend using getServerSession as per
   //here: https://next-auth.js.org/configuration/nextjs#getserversession
   const session = await getServerSession(ctx.req, ctx.res, authOptions)
-  if (!session) {
-    return {
-      redirect: {
-        destination: routes.root,
-        permanent: false,
-      },
-    }
-  }
   return { props: { session } }
 }
 
