01da4c7054b0469abd6d6aff984614eb82e0c2f7
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2023-03-23 00:30:31 +0200

    create membership (invite users) mutation

--- src/components/popovers/ShareFileDialog/InvitePeopleTab.tsx
+++ src/components/popovers/ShareFileDialog/InvitePeopleTab.tsx
@@ -12,11 +12,25 @@ import Box from '@mui/system/Box'
 import Stack from '@mui/system/Stack'
 import { FileRoles, User } from '@prisma/client'
 import axios, { AxiosResponse } from 'axios'
-import { useState } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 import { useQuery } from 'react-query'
 
-type SimpleUser = Omit<User, 'password'>
-export default function InvitePeopleTab({ fileId }: { fileId: string }) {
+export type SimpleUser = Omit<User, 'password'>
+
+type InvitePeopleTabProps = {
+  fileId: string
+  user: SimpleUser | null
+  role: FileRoles
+  setUser: Dispatch<SetStateAction<SimpleUser | null>>
+  setRole: Dispatch<SetStateAction<FileRoles>>
+}
+export default function InvitePeopleTab({
+  fileId,
+  user,
+  role,
+  setUser,
+  setRole,
+}: InvitePeopleTabProps) {
   const { data: users, isLoading } = useQuery(['available-users', fileId], async () => {
     const { data } = await axios.get<unknown, AxiosResponse<SimpleUser[]>>(
       `/api/file/${fileId}/available-users`
@@ -24,8 +38,6 @@ export default function InvitePeopleTab({ fileId }: { fileId: string }) {
     return data
   })
   const [openAutocomplete, setAutocompleteOpen] = useState(false)
-  const [selectedUserId, setSelectedUserId] = useState<SimpleUser | null>(null)
-  const [selectedUserRole, setSelectedUserRole] = useState<FileRoles>('VIEWER')
 
   return (
     <Stack direction="row" gap={2}>
@@ -39,7 +51,7 @@ export default function InvitePeopleTab({ fileId }: { fileId: string }) {
         filterSelectedOptions
         getOptionLabel={(option) => option.email}
         onChange={(_, value) => {
-          setSelectedUserId(value)
+          setUser(value)
         }}
         isOptionEqualToValue={(option, value) => option.email === value.email}
         renderInput={(params) => (
@@ -58,18 +70,16 @@ export default function InvitePeopleTab({ fileId }: { fileId: string }) {
           />
         )}
       />
-      {selectedUserId && selectedUserRole && (
+      {user && (
         <Box minWidth={140}>
           <FormControl fullWidth>
             <InputLabel id="select-user-role">Role</InputLabel>
             <Select
               labelId="select-user-role"
               id="select-user-role"
-              value={selectedUserRole}
+              value={role}
               label="Role"
-              onChange={(event: SelectChangeEvent) =>
-                setSelectedUserRole(event.target.value as FileRoles)
-              }
+              onChange={(event: SelectChangeEvent) => setRole(event.target.value as FileRoles)}
             >
               <MenuItem value="VIEWER">Viewer</MenuItem>
               <MenuItem value="EDITOR">Editor</MenuItem>
