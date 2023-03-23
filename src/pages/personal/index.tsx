ec750cb947e611f76bc4eb3ec0ebde4596c7fa96
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2023-03-23 15:59:37 +0200

    remove serverSideSession from personal/private files page

--- src/pages/personal/index.tsx
+++ src/pages/personal/index.tsx
@@ -2,17 +2,7 @@ import FilesTable from '@/components/FIlesTable'
 import { getLayout } from '@/layouts/MainLayout'
 import { FileVisibility } from '@prisma/client'
 import axios, { AxiosResponse } from 'axios'
-import { getServerSession } from 'next-auth'
-import { GetServerSideProps } from 'next/types'
 import { useQuery } from 'react-query'
-import { authOptions } from '../api/auth/[...nextauth]'
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  //For getting session on server side the docs recommend using getServerSession as per
-  //here: https://next-auth.js.org/configuration/nextjs#getserversession
-  const session = await getServerSession(ctx.req, ctx.res, authOptions)
-  return { props: { session } }
-}
 
 export type FileWithoutContent = {
   id: string
