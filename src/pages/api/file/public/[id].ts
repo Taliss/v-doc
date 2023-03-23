c95487914ddda8520a96627eec8e3f78857d3049
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2023-03-23 16:41:46 +0200

    add public endpoint for fetching single file with public data

---
+++ src/pages/api/file/public/[id].ts
@@ -0,0 +1,32 @@
+import prisma from '@/prisma-client'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { ParsedUrlQuery } from 'querystring'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    const { id } = req.query as ParsedUrlQuery & { id: string }
+
+    try {
+      const file = await prisma.file.findUnique({
+        where: {
+          id,
+        },
+        include: {
+          owner: {select: {id:true, email: true}}
+        }
+      })
+
+      if (!file || file.visibility !== 'PUBLIC') {
+        return res.status(404).end()
+      }
+
+      return res.json(file)
+
+    } catch (error) {
+      console.error(error)
+      return res.status(500).end()
+    }
+  }
+
+  res.status(405).end()
+}
