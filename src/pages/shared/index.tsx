f72cda4e39520460a63408e54aa887b967d612ea
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2023-03-23 15:37:49 +0200

    remove serverSideSession inject as it should be done by the middleware

--- src/pages/shared/index.tsx
+++ src/pages/shared/index.tsx
@@ -2,18 +2,8 @@ import FilesTable from '@/components/FIlesTable'
 import { getLayout } from '@/layouts/MainLayout'
 import { FileRoles } from '@prisma/client'
 import axios, { AxiosResponse } from 'axios'
-import { getServerSession } from 'next-auth'
-import { GetServerSideProps } from 'next/types'
 import { FileWithoutContent } from 'pages/personal'
 import { useQuery } from 'react-query'
-import { authOptions } from '../api/auth/[...nextauth]'
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  //For getting session on server side the docs recommend using getServerSession as per
-  //here: https://next-auth.js.org/configuration/nextjs#getserversession
-  const session = await getServerSession(ctx.req, ctx.res, authOptions)
-  return { props: { session } }
-}
 
 type SharedFile = {
   fileId: string
@@ -24,7 +14,7 @@ type SharedFile = {
 }
 
 export default function Shared() {
-  const { data: files } = useQuery('private-files', async () => {
+  const { data: files } = useQuery('shared-files', async () => {
     const { data } = await axios.get<unknown, AxiosResponse<SharedFile[]>>('/api/file/shared')
     return data.map(({ file }) => file) || []
   })
