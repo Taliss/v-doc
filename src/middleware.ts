9b0c1d0cf097a66816e4fefc81d9e13da96a5d7c
Iliyan Vlahov <iliyan.vlahov@gmail.com>
2023-03-24 12:47:25 +0200

    protect shared file page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from 'next-auth/middleware'
 
-export const config = { matcher: ['/personal/:id*', '/shared'] }
+export const config = { matcher: ['/personal/:id*', '/shared/:id*'] }
